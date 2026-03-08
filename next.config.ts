05241f6f1f2a13a090475ec0396cedf57a687de8
mituwo-320 <nishio.kousei@gmail.com>
2026-03-08 13:09:12 +0900

    fix: revert custom prisma output to fix Vercel deploy size error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Prisma binaries can cause the Serverless Function size to exceed Vercel's 250MB uncompressed limit.
+  // This tells Next.js to not bundle these large engines into every API route.
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '../generated/client'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Prisma client type might show EPERM locally, but it works on Vercel
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
